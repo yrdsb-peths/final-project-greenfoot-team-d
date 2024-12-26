ad7a58e0f0fd895662624b92c8a63ff719513181
glorianaz <150281309+glorianaz@users.noreply.github.com>
2024-12-26 00:38:24 -0500

    Changes

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -13,16 +13,24 @@ public class InstructionScreen extends World
      * Constructor for objects of class InstructionScreen.
      * 
      */
-    public InstructionScreen()
+    private String[] allText;
+    private int currentIndex = 0;
+    private Label instructionLabel; 
+    public class InstructionScreen extends World 
     {    
+        super(600, 400, 1);
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         private World home;
-        String firstInstruction = "Eat as much food as possible without \n crashing into walls or yourself.";
-        String secondInstruction = "The snake will grow depending on how much food you eat";
-        String thirdInstruction = "The difficulty of the game will get harder \n as the levels increase";
-        String fourthInstruction = "Use Arrow Keys to control the snake's movement";
-        private String[] allText = { firstInstruction, secondInstruction, thirdInstruction, fourthInstruction };
+        
+         llText = new String[] {
+            "Eat as much food as possible without \n crashing into walls or yourself.",
+            "The snake will grow depending on how much food you eat",
+            "The difficulty of the game will get harder \n as the levels increase",
+            "Use Arrow Keys to control the snake's movement"
+        };
+        
         private int currentIndex = 0;
         private Label instructionLabel = new Label(allText[currentIndex], 40, true);
+        addObject(instructionLabel, getWidth() / 2, getHeight() / 2);
     }
 }
