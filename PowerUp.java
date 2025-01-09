61b294c80dc8da619d37f11a0ce29d63f4ba0871
glorianaz <150281309+glorianaz@users.noreply.github.com>
2025-01-09 13:05:55 -0500

    Power Up Class Created

---
+++ PowerUp.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class PowerUp here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class PowerUp extends Actor
+{
+     public PowerUp() {
+        GreenfootImage image = new GreenfootImage("powerup.png");
+        setImage(image);
+    }
+    
+    /**
+     * Act - do whatever the PowerUp wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
