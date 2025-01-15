ad18f300d87fc532ea77945fc77f3d87b5de3266
tiiffanyho <Tiffanyho0903@gmail.com>
2025-01-15 12:25:48 -0500

    new colors for body

--- SnakeBody.java
+++ SnakeBody.java
@@ -2,8 +2,13 @@ import greenfoot.*;
 
 public class SnakeBody extends Snake {
     
-    public SnakeBody() {
+    public SnakeBody(String selectedColor) {
         GreenfootImage yellowBody = new GreenfootImage("images/png/snake_yellow_blob_32.png");
+        GreenfootImage pinkBody = new GreenfootImage("images/body/pinkBody.png");
+        GreenfootImage redBody = new GreenfootImage("images/body/redBody.png");
+        GreenfootImage purpleBody = new GreenfootImage("images/body/purpleBody.png");
+        GreenfootImage orangeBody = new GreenfootImage("images/body/orangeBody.png");
+        GreenfootImage greenBody = new GreenfootImage("images/body/greenBody.png");
         setImage(yellowBody);
     }
 }
